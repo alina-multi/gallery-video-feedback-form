2ffadddb38f60dcfb98878eb82b4f60656f21d3d
Alina Akseninko <79420032+alina-multi@users.noreply.github.com>
2022-08-21 14:04:29 +0100

    update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,40 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
-const STORAGE_KEY = "videoplayer-current-time";
-
-
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function(data) {
-    localStorage.setItem(STORAGE_KEY, data.seconds);
+
+const storage = localStorage.getItem('videoplayer-current-time');
+
+    player.on(
+        'timeupdate', throttle(function () {
+          player.getCurrentTime()
+            .then(function (seconds) {
+              localStorage.setItem('videoplayer-current-time', seconds);
+            })
+            .catch(function (error) {});
+        }, 1000),
+        player
+          .setCurrentTime(storage)
+          .then(function (seconds) {})
+          .catch(function (error) {})
+      );
+
+
+
+
+
+// const STORAGE_KEY = "videoplayer-current-time";
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// player.on('timeupdate', function(data) {
+//     localStorage.setItem(STORAGE_KEY, data.seconds);
     
-});
+// });
     
-const currentTime = localStorage.getItem(STORAGE_KEY);
-player.setCurrentTime(currentTime)
+// const currentTime = localStorage.getItem(STORAGE_KEY);
+// player.setCurrentTime(currentTime)
 
 ///когда видео заканчиваеться удаляеться ключ с localStorage и видео начинаеться с начала ?????????
 
